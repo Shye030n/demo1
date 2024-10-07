295c83e94a5e6bbce85356ed12aebff4c07448e7
shye030n <shyeo_on@naver.com>
2024-10-07 10:54:59 +0900

    환경설정 완료

---
+++ springDemo/src/main/java/com/sh/springdemo/sample/SampleDTO.java
@@ -0,0 +1,21 @@
+package com.sh.springdemo.sample;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SampleDTO {
+    private String name;
+    private int age;
+    private String[] hobby;
+
+
+    }
+
+
+
+
