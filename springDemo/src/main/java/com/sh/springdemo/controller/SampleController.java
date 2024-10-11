7a61a5e7efc0d83fc721c5fd7341e3a3c804b0e8
shye030n <shyeo_on@naver.com>
2024-10-11 12:03:14 +0900

    2024/10/10

--- springDemo/src/main/java/com/sh/springdemo/controller/SampleController.java
+++ springDemo/src/main/java/com/sh/springdemo/controller/SampleController.java
@@ -19,6 +19,7 @@ public class SampleController {
         return "hello";
     }
     @GetMapping("/ex1")
+    //ex1을 선택하면 이 함수 선택 됨. 다른 것들도 매한가지.
     // view로 데이터 전달
     // ex1.jsp 와 연결되는 부분 <%-- 이게 파라미터를 보내는 과정 --%>
     // http://localhost:8080/ex1?name=aaa&age=10&gender=m 이거 쳐서 잘 되는지 확인
@@ -32,6 +33,7 @@ public class SampleController {
         log.info(age);
         log.info(gender);
         model.addAttribute("name", name);
+        // model.은 views 폴더 ex1.jsp로 연결.
         model.addAttribute("age", age);
         model.addAttribute("gender", gender);
         //Attribute 속성
@@ -39,7 +41,9 @@ public class SampleController {
 
     @GetMapping("/ex2/{name}")
     // http://localhost:8080/ex2/aaa 이거 쳐서 잘 되는 지 확인
+    //@PathBariable 경로 변수 , aaa가 이름 위치
     public void ex2(@PathVariable("name") String name){
+
         log.info("ex2");
         log.info(name);
     }
@@ -63,11 +67,25 @@ public class SampleController {
         redirectAttributes.addAttribute("name", "aaaa");
         //addAttribute는 웹 페이지의 첫 화면에도 데이터가 있고, F5를 눌러도 데이터가 그대로 존재함.
         return "redirect:/ex5";
+        //redirect: 를 설정하고 return하면 (jsp파일을 찾는 것이 아닌)
+        // 다시 컨트롤러로 재 요청함.
+
     }
     @GetMapping("/ex5")
     public void ex5(@RequestParam("name") String name, Model model){
         log.info("ex5");
         log.info(name);
         model.addAttribute("name", name);
+    }
+
+    @GetMapping("/ex06")
+    //CommonExceptionAdvice.java랑 비교
+    //http://localhost:8080/ex6?p1=a&p2=5 여기로 접속
+    //http://localhost:8080/ex6?p1=a&p2=b 이렇게 치면 에러나야되는데 나는 왜 에러가 안나지..?
+    public void ex6(@RequestParam ("p1") String p1,
+                    @RequestParam("p2") int p2){
+        log.info("ex6");
+        log.info(p1);
+        log.info(p2);
     }
 }
